bb942047d66282dd111f1cf10ae14105d196e007
ruhtar <arthur.amorim10@gmail.com>
2022-10-27 21:17:07 -0300

    Department entity class

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department dp = new Department(1, "opa");
+        System.out.println(dp);
+    }
+}
---
+++ src/main/java/model/entities/Seller.java
@@ -0,0 +1,13 @@
+package model.entities;
+
+import java.sql.Date;
+
+public class Seller {
+    Integer id;
+    String name;
+    String email;
+    Date birthDate;
+    Double baseSalary;
+
+
+}
