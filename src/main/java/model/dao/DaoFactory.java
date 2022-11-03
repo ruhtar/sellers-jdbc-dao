507395abc59ba8f98f63856935aa8dbb1685d750
ruhtar <arthur.amorim10@gmail.com>
2022-11-02 22:08:16 -0300

    Comentário apenas

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -5,7 +5,7 @@ import model.dao.impl.SellerDAOJDBC;
 import model.entities.Seller;
 
 public class DaoFactory {
-
+    //Factory serve apenas serve para instanciar um DAO
     public static SellerDAO createSellerDAO(){
         return new SellerDAOJDBC(DB.getConnection());
     }
